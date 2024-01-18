f100ddd0bf7ec3cf1a1e0944a0c0dd731c9fba8a
David Rodriguez <santiago1.dsrr@gmail.com>
2024-01-17 19:30:34 -0500

    added pages .jsx files

---
+++ src/Pages/AboutMe.jsx
@@ -0,0 +1,19 @@
+export default function AboutMe() {
+    return (
+      <div>
+            <h2 className='pageTitle'>About me</h2>
+            <div id="infoPicture" className="content">
+                <div id='image'>
+                    <img src="src\assets\picture.jpg" alt="Picture - David Rodriguez" id='picture'/>
+                </div>
+                <p>
+                    My name is David Santiago Rodriguez, thank you for visiting my portafolio. I am a 23 year old Colombian, currently taking a coding bootcamp at the University of Toronto.
+                    I have a background of Mechanical Engineering studies at Conestoga College and 2 years of work experiance as a Drafting and Quality Technician at Planitar Inc.
+                    I finally give myself the change to learn to code. It rapidly became one of my passions, and I invite you to scroll down and see what I have learn so far. 
+                </p>
+            </div>
+      </div>
+    );
+  }
+  
+
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+      <div>
+        <h2 className='pageTitle'>Contact</h2>
+      </div>
+    );
+  }
