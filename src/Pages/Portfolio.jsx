88fe7e4079332f8950e578ddef1d9ece80d9a48d
David Rodriguez <santiago1.dsrr@gmail.com>
2024-01-18 23:04:57 -0500

    added the content and styling of the project comp.

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,13 +1,15 @@
 import Project from "../Project"
-
+import MovieSelector from "../assets/GameOverPage.png"
+import codingQuiz from "../assets/Quiz.png"
+import NoteTaker from "../assets/NoteTaker.png"
 export default function Portfolio() {
     return (
       <>
         <h2 className='pageTitle'>Portfolio</h2>
-        <div style={{display:'Flex', justifyContent: 'center'}}>
-          <Project image="" title="Movie Selector Game" link="https://github.com/DavidRodriguez119/Movie-Selector-Game.git"/>
-          <Project image="" title="Boardgame Cafe" link="https://github.com/r-basu/boardgame-cafe.git"/>
-          <Project image="" title="Note Taker" link="https://github.com/DavidRodriguez119/Note-taker-app.git"/>
+        <div style={{display:'Flex', justifyContent: 'center', flexWrap: `wrap`}}>
+          <Project image={MovieSelector} deployed="https://davidrodriguez119.github.io/Movie-Selector-Game/" title="Movie Selector Game" link="https://github.com/DavidRodriguez119/Movie-Selector-Game.git"/>
+          <Project image={codingQuiz} deployed="https://davidrodriguez119.github.io/Coding-Quiz/" title="Coding-Quiz" link="https://github.com/DavidRodriguez119/Coding-Quiz.git"/>
+          <Project image={NoteTaker} deployed="https://note-taking-app-d-5a75575cca1a.herokuapp.com/" title="Note Taker" link="https://github.com/DavidRodriguez119/Note-taker-app.git"/>
         </div>
       </>
     );
--- src/Project/index.jsx
+++ src/Project/index.jsx
@@ -4,10 +4,19 @@ import "./style.css";
 export default function Project(props) {
     return(
         <>
-        <div className="project" style={{
-        }}>
-            
-        </div>
+            <div className="project" style={{
+                backgroundImage: `url(${props.image})`,
+                backgroundSize: `cover`,
+                backgroundRepeat: `no-repeat`,
+                backgroundPosition: `center`,
+            }}>
+                <div id="projectContent">
+                    <h3>{props.title}</h3>
+                    <a href={props.link}><img src="src\assets\github.png" alt="github logo" className="logo"/></a><br />
+                    <a href={props.deployed}>Deployed Link</a>
+                </div>
+                
+            </div>
         </>
     )
 }
